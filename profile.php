47d52f2a2d996da97a47414ae8e08698b3a83011
shubham lathiiya <90539853+shubhhamlathiya@users.noreply.github.com>
2023-02-11 18:10:44 +0530

    7 commit

--- profile.php
+++ profile.php
@@ -102,20 +102,56 @@
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">Mobile Number</span>
                                 </div>
-                                <input type="text" name="MobileNo" class="form-control MobileNo" placeholder="Mobile Number">
+                                <input type="text" name="MobileNo" id="MobileNo" class="form-control MobileNo" placeholder="Mobile Number" required maxlength="10">
                             </div>
+                            <script>
+                                $(document).ready(function () {
+                                    $('#MobileNo').on('keypress', function (e) {
+                                        var $this = $(this);
+                                        var regex = new RegExp("^[0-9\b]+$");
+                                        var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+                                        // for 10 digit number only
+                                        if ($this.val().length > 9) {
+                                            e.preventDefault();
+                                            return false;
+                                        }
+                                        if (e.charCode < 54 && e.charCode > 47) {
+                                            if ($this.val().length == 0) {
+                                                e.preventDefault();
+                                                return false;
+                                            } else {
+                                                return true;
+                                            }
+                                        }
+                                        if (regex.test(str)) {
+                                            return true;
+                                        }
+                                        e.preventDefault();
+                                        return false;
+                                    });
+                                });
+                            </script>
                             <div class="input-group form-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">Date of Birth</span>
                                 </div>
-                                <input type="date" name="DOB" id="DOB" class="form-control">
+                                <input type="date" name="DOB" id="DOB" class="form-control" placeholder="dd-mm-yyyy" required>
                             </div>
-
+                            <script>
+                                $(document).ready(function () {
+                                    $("#DOB").datepicker({
+                                        changeMonth: true,
+                                        changeYear: true,
+                                        yearRange: "1980",
+                                        maxDate: "-18y"
+                                    });
+                                });
+                            </script>
                             <div class="input-group form-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">Driving Licence</span>
                                 </div>
-                                <input type="text" name="licence_number" id="licence_number" class="form-control licence_number" placeholder="GJ0000000000000" maxlength="15">
+                                <input type="text" name="licence_number" id="licence_number" class="form-control licence_number" placeholder="GJ0000000000000" maxlength="15" required>
                             </div>
                             <span id="ErrorR_no" style="color: #f50404;"></span>
 
@@ -123,9 +159,35 @@
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">Aadhar No</span>
                                 </div>
-                                <input type="text" name="Aadharcard" class="form-control Aadharcard" placeholder="000000000000">
+                                <input type="text" name="Aadharcard" class="form-control Aadharcard" placeholder="000000000000" required>
                             </div>
-
+                            <script>
+                                $(document).ready(function () {
+                                    $('.Aadharcard').on('keypress', function (e) {
+                                        var $this = $(this);
+                                        var regex = new RegExp("^[0-9\b]+$");
+                                        var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+                                        // for 10 digit number only
+                                        if ($this.val().length > 11) {
+                                            e.preventDefault();
+                                            return false;
+                                        }
+                                        if (e.charCode < 50 && e.charCode > 47) {
+                                            if ($this.val().length == 0) {
+                                                e.preventDefault();
+                                                return false;
+                                            } else {
+                                                return true;
+                                            }
+                                        }
+                                        if (regex.test(str)) {
+                                            return true;
+                                        }
+                                        e.preventDefault();
+                                        return false;
+                                    });
+                                });
+                            </script>
                             <div class="form-group" align="center">
                                 <input type="submit" name="AddCustomer" value="Save" class="btn login_btn">
                             </div>
@@ -133,125 +195,41 @@
                     </div>
                     <script>
                         $(document).ready(function () {
-                            $('.MobileNo').on('keypress', function (e) {
-                                var $this = $(this);
-                                var regex = new RegExp("^[0-9\b]+$");
-                                var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
-                                // for 10 digit number only
-                                if ($this.val().length > 9) {
-                                    e.preventDefault();
-                                    return false;
-                                }
-                                if (e.charCode < 54 && e.charCode > 47) {
-                                    if ($this.val().length == 0) {
-                                        e.preventDefault();
-                                        return false;
-                                    } else {
-                                        return true;
+                            $("#licence_number").keyup(function (e) {
+                                $("#ErrorR_no").html('');
+
+                                var validstr = '';
+                                var dInput = $(this).val();
+                                var numpattern = /^\d+$/;
+                                var alphapattern = /^[A-Z]+$/;
+
+                                for (var i = 0; i < dInput.length; i++) {
+
+                                    if ((i == 2 || i == 3 || i == 4 || i == 5 || i == 6 || i == 7 || i == 8 || i == 9 || i == 10 || i == 11 || i == 12 || i == 13 || i == 14 || i == 15)) {
+                                        if (numpattern.test(dInput[i])) {
+                                            // console.log('validnum' + dInput[i]);
+                                            validstr += dInput[i];
+                                        } else {
+                                            $("#ErrorR_no").html("Only Digits").show();
+
+                                        }
                                     }
+
+                                    if ((i == 0 || i == 1)) {
+                                        if (alphapattern.test(dInput[i])) {
+                                            // console.log('validword' + dInput[i]);
+                                            validstr += dInput  [i];
+                                        } else {
+                                            $("#ErrorR_no").html("Only Capital Alpahbets").show();
+
+                                        }
+                                    }
+
                                 }
-                                if (regex.test(str)) {
-                                    return true;
-                                }
-                                e.preventDefault();
+
+                                $(this).val(validstr);
                                 return false;
                             });
-                        });
-
-                        $(document).ready(function () {
-                            $('.Aadharcard').on('keypress', function (e) {
-                                var $this = $(this);
-                                var regex = new RegExp("^[0-9\b]+$");
-                                var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
-                                // for 10 digit number only
-                                if ($this.val().length > 11) {
-                                    e.preventDefault();
-                                    return false;
-                                }
-                                if (e.charCode < 50 && e.charCode > 47) {
-                                    if ($this.val().length == 0) {
-                                        e.preventDefault();
-                                        return false;
-                                    } else {
-                                        return true;
-                                    }
-                                }
-                                if (regex.test(str)) {
-                                    return true;
-                                }
-                                e.preventDefault();
-                                return false;
-                            });
-                        });
-                        // $(document).ready(function () {
-                        //     $('.licence_number').on('keypress', function (e) {
-                        //         var $this = $(this);
-                        //         var regex = new RegExp("^[A-Z]+[0-9\b]+$");
-                        //         var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
-                        //         // for 10 digit number only
-                        //         if ($this.val().length > 15) {
-                        //             e.preventDefault();
-                        //             return false;
-                        //         }
-                        //         if (e.charCode < 72 && e.charCode > 70) {
-                        //             if ($this.val().length == 1) {
-                        //                 e.preventDefault();
-                        //                 return false;
-                        //             } else {
-                        //                 return true;
-                        //             }
-                        //         }
-                        //         if (e.charCode < 75 && e.charCode > 73) {
-                        //             if ($this.val().length == 0) {
-                        //                 e.preventDefault();
-                        //                 return false;
-                        //             } else {
-                        //                 return true;
-                        //             }
-                        //         }
-                        //         if (regex.test(str)) {
-                        //             return true;
-                        //         }
-                        //         e.preventDefault();
-                        //         return false;
-                        //     });
-                        // });
-                        $(document).ready(function () {
-                        $("#licence_number").keyup(function (e) {
-                            $("#ErrorR_no").html('');
-
-                            var validstr = '';
-                            var dInput = $(this).val();
-                            var numpattern = /^\d+$/;
-                            var alphapattern = /^[A-Z]+$/;
-
-                            for (var i = 0; i < dInput.length; i++) {
-
-                                if ((i == 2 || i == 3 || i == 4 || i == 5 || i == 6 || i == 7 || i == 8 || i == 9 || i == 10 || i == 11 || i == 12 || i == 13 || i == 14 || i == 15)) {
-                                    if (numpattern.test(dInput[i])) {
-                                        // console.log('validnum' + dInput[i]);
-                                        validstr += dInput[i];
-                                    } else {
-                                        $("#ErrorR_no").html("Only Digits").show();
-
-                                    }
-                                }
-
-                                if ((i == 0 || i == 1 )) {
-                                    if (alphapattern.test(dInput[i])) {
-                                        // console.log('validword' + dInput[i]);
-                                        validstr += dInput  [i];
-                                    } else {
-                                        $("#ErrorR_no").html("Only Capital Alpahbets").show();
-
-                                    }
-                                }
-
-                            }
-
-                            $(this).val(validstr);
-                            return false;
-                        });
                         });
 
                     </script>
@@ -266,9 +244,7 @@
                         $Aadharcard = $_POST['Aadharcard'];
 
                         $customer = $conn->prepare("INSERT INTO customer VALUES (?,?,?,?,?,?,?)");
-//                        $customer = $conn->prepare("UPDATE customer SET Name=?,MobileNo=?,Date_of_birth=?,Driving_licence=?,AadharCard_no=? WHERE Email =?");
                         $customer->bind_param("sssssss", $fullname, $Emailid, $password, $mobile, $DOB, $licence_number, $Aadharcard);
-//                        $customer->bind_param("ssssss", $fullname, $mobile, $DOB, $licence_number, $Aadharcard, $Emailid);
                         $Addcustomer = $customer->execute();
                         if ($Addcustomer > 0) {
                             echo "<script>window.location.href='index.php'</script>";
@@ -280,5 +256,9 @@
                 </div>
             </div>
         </div>
+<!--        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js" type="text/javascript"></script>-->-->-->
+<!--                        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js" type="text/javascript"></script>-->
+<!--                                <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/start/jquery-ui.css" rel="Stylesheet" type="text/css" />-->
+
     </body>
 </html>
